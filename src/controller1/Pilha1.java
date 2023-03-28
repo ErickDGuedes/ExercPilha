6c70f9291fbc0c10e620d7f44f6abb97784d2cad
erick <erick@DEKU-PC>
2023-03-28 11:52:11 -0300

    Commit

---
+++ src/controller1/Pilha1.java
@@ -0,0 +1,67 @@
+package controller1;
+
+import br.edu.fateczl.pilhaint.Pilha;
+
+public class Pilha1 {
+
+	public Pilha1() {
+		super();
+
+	}
+
+	public void ExercA() {
+		Pilha p = new Pilha();
+		for(int i=0 ; i<10 ; i++) {
+			if(i % 2 == 0) {
+				p.push(i*i);
+			}else {
+				if(i <= 5) {
+					p.push(i);
+				}else {
+					try {
+						p.pop();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				try {
+					p.top();
+					System.out.println(p.size());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				p.size(); 
+			}
+		}
+		//System.out.println(p.size());
+	}
+
+	public void ExercB() {
+		Pilha p = new Pilha();
+		for(int i=100 ; i<115 ; i++) {
+			if(i % 2 == 0) {
+				p.push(i+100);
+			}else {
+				if(p.size() <= 4) {
+					p.push(i+50);
+				}else {
+					try {
+						p.pop();
+						
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				try {
+					p.top();
+					System.out.println(p.size());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				p.size(); 
+			}
+		}
+		//System.out.println(p.size());
+	}
+	
+}
---
+++ src/view1/Principal1.java
@@ -0,0 +1,15 @@
+package view1;
+
+import controller1.Pilha1;
+
+public class Principal1 {
+
+	public static void main(String[] args) {
+		Pilha1 p = new Pilha1();
+		int x = 1;
+		p.ExercA();
+		System.out.println();
+		p.ExercB();
+	}
+
+}
